482a82d020cd45add44a645aeee1156321c1573c
Kolabdul <kolabdul00@gmail.com>
2026-08-01 16:42:28 +0100

    Two Sum Soln

--- 0001_two_sum.py
+++ 0001_two_sum.py
@@ -1,11 +1,13 @@
+
 nums = [2, 7, 11, 15]
 target = 9
 
 class Solution:
-    def twoSums(sdelf, nums, target):
-         notebook = {}
-         for i, n in enumerate(nums):
-              diff = target - n
-              if diff in notebook:
-                   return [notebook[diff], i]
-              notebook[n] = 1 
+    def twoSum(self, nums, target):
+        notebook = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in notebook:
+                return [notebook[diff], i]
+            notebook[n] = i
+
